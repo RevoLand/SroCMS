019317b0fb99aa3c7820528f5005bdcdc8272ef8
Mert Ercan <revoland@windowslive.com>
2019-10-26 21:38:38 +0300

    "Beni Hatırla" fonksiyonu için gereken TB_User tablosu düzenlemesi

---
+++ database/migrations/2019_10_26_183515_add_remember_token_to_tb_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRememberTokenToTbUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('account')->table('TB_User', function (Blueprint $table) {
+            $table->string('remember_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('account')->table('TB_User', function (Blueprint $table) {
+            $table->dropColumn('remember_token');
+        });
+    }
+}
