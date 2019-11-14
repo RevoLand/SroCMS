3c834f8cef90b126f4abd946b60218c0aa34b58e
Mert Ercan <revoland@windowslive.com>
2019-11-15 01:41:20 +0300

    Update MenuController.php

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -8,12 +8,12 @@ class MenuController extends Controller
 {
     public static function getMenus($location = null, $limit = null)
     {
-        return dd(Menu::whereEnabled(true)->where(function ($query) use ($location)
+        return Menu::whereEnabled(true)->where(function ($query) use ($location)
         {
             if (isset($location))
             {
                 $query->whereLocation($location);
             }
-        })->orderBy('order')->limit($limit));
+        })->orderBy('order')->limit($limit)->get();
     }
 }
