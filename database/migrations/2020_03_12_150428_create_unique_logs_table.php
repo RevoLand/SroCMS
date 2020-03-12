9c6ecb8969ef790a22cd360290ec47afaacf5c60
Mert Ercan <revoland@windowslive.com>
2020-03-12 15:13:39 +0300

    Create 2020_03_12_150428_create_unique_logs_table.php

---
+++ database/migrations/2020_03_12_150428_create_unique_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUniqueLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::connection('account')->create('unique_logs', function (Blueprint $table)
+        {
+            $table->bigIncrements('id');
+            $table->string('UniqueCodeName');
+            $table->integer('CharacterID');
+            $table->string('CharacterName');
+            $table->integer('UserJID');
+            $table->smallInteger('RegionId');
+            $table->double('PosX');
+            $table->double('PosY');
+            $table->double('PosZ');
+            $table->smallInteger('WorldID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::connection('account')->dropIfExists('unique_logs');
+    }
+}
